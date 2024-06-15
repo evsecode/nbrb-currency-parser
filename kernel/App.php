080cb09ea8bdff5e469b44d22101d14c385859c7
evsecode <mynameisevse@gmail.com>
2024-06-15 19:01:39 +0300

    added kernel/Container.

--- kernel/App.php
+++ kernel/App.php
@@ -2,20 +2,24 @@
 
 namespace App\Kernel;
 
+use App\Kernel\Container\Container;
 use App\Kernel\Http\Request;
 use App\Kernel\Router\Router;
 
 class App
 {
+    private Container $container;
 
+    public function __construct()
+    {
+        $this->container = new Container();
+    }
     public function run(): void
     {
-        $uri = $_SERVER['REQUEST_URI'];
-        $method = $_SERVER['REQUEST_METHOD'];
-
-        $router = new Router();
-        $request = Request::createFromGlobals();
-
-        $router->dispatch($request->uri(), $request->method());
+        $this->container
+            ->router->
+            dispatch(
+                $this->container->request->uri(),
+                $this->container->request->method());
     }
 }
